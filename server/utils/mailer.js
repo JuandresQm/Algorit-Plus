3f5bf0a33a82717d867029d6f6944bbf0c0487c2
JuandresQm <juandresmachado@outlook.es>
2026-07-15 13:15:25 -0400

    Update mailer.js

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -7,6 +7,8 @@ const transporter = nodemailer.createTransport({
   pool: true,              
   family: 4,               
   connectionTimeout: 10000,
+  debug: true,
+  logger: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
