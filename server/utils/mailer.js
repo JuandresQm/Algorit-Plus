59665a49facaf15adfcc22a9b596b4ca14cd3a64
JuandresQm <juandresmachado@outlook.es>
2026-07-15 13:51:35 -0400

    Update mailer.js Brevo

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,21 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: '173.194.203.108',
-  port: 465,
-  secure: true,
-  pool: true,              
-  family: 4,               
-  connectionTimeout: 10000,
-  debug: true,
-  logger: true,
+  host: 'smtp-relay.brevo.com',
+  port: 587,    
+  secure: false,      
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-    servername: 'smtp.gmail.com' 
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS        
   }
 });
 
